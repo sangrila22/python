b9eb2d56abef5ecc689943524d0c46a3236e1a9a
Irma Grigiene <irma.grigiene@bazaarvoice.com>
2018-10-25 12:10:52 +0300

    MOD01_1-1.1_Intro_Python

--- MOD01_1-1.1_Intro_Python.py
+++ MOD01_1-1.1_Intro_Python.py
@@ -1,2 +1,7 @@
-print('Irma')
-print('is using Python!')
+# create an original "Hello World" style message
+print("Hello World")
+# edit cell: add print() with the message "after edit, save!
+print("after edit, save!") 
+#edit cell: add print() with the message showing the keyboard Shortcut to save Ctrl + s
+run the cell
+print("Ctrl + s")
